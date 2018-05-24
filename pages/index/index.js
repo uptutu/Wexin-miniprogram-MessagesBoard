48dd7866d6c80f12ff16d8ded795802299eb1983
Yattao Kung <alexkung@outlook.com>
2018-05-24 15:00:05 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,44 @@
+//index.js
+
+
+Page({
+    data: {
+        messages: [
+            
+        ]
+
+    },
+    onLoad: function(){
+        var that = this
+        wx.request({
+            url: 'http://localhost/mini/data.php',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            success: function (res) {
+                console.log(res.data)
+                that.setData({
+                    messages: res.data
+                })
+            }
+        })
+    },
+    formSubmit: function (e) {
+        var data = e.detail.value
+        data.title = "User";
+
+        wx.request({
+            url: 'http://localhost/mini/input.php',
+            method: 'POST',
+            data: data,
+            header: {
+                'Content-Type': 'application/json'
+            },
+            success: function (res) {
+                console.log(data)
+                this.onLoad
+            }
+        })
+        
+    }
+});
